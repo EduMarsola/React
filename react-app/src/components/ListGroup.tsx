bceb61a82aafa8ba087b3c89eec8124bb45ce664
EduMarsola <eduimarsola@gmail.com>
2025-12-12 19:20:21 -0300

    12/12/25 - new ifs, SyntheticBaseEvent, EventHandler, Type Adaptation, Hook, destruture

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -28,15 +28,66 @@ function ListGroupStatic() {
   );
 }
 
-function ListGroup() {
+function ListGroupArrowFunction() {
   let items = [ 'NY', 'SF', 'TK', 'SP' ]
   items = []
+  const getMessage = () => {
+    return items.length === 0 ? <p>No Items Found</p> : null
+  }
   return(
       <>
         <h1>Hello</h1>
-        { items.length === 0 ? <p>No Items Found</p> : null} 
+        { getMessage() } 
         <ul className="list-group">
             {items.map(item => <li key={item}>{item}</li>)} 
+        </ul>
+    </>
+  );
+}
+
+import { useState, type MouseEvent } from "react";
+function ListGroupEventHandler() {
+  let items = [ 'NY', 'SF', 'TK', 'SP' ]
+  let selectedIndex = -1;
+  //eventHandler
+  const handleClick = (event : MouseEvent) => console.log(event);
+  return(
+      <>
+        <h1>Hello</h1>
+        { items.length === 0 && <p>No Item Found</p>}
+        <ul className="list-group">
+            {items.map((item, index) => 
+              <li 
+                className={ selectedIndex === index 
+                            ? "list-group-item active" 
+                            : "list-group-item"} 
+                key={item} 
+                onClick={handleClick}>{item}
+              </li>)
+            } 
+        </ul>
+    </>
+  );
+}
+
+function ListGroup() {
+  let items = [ 'NY', 'SF', 'TK', 'SP' ]
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+  useState("",)
+  return(
+      <>
+        <h1>Hello</h1>
+        { items.length === 0 && <p>No Item Found</p>}
+        <ul className="list-group">
+            {items.map((item, index) => 
+              <li 
+                className={ selectedIndex === index 
+                            ? "list-group-item active" 
+                            : "list-group-item"} 
+                key={item} 
+                onClick={() => setSelectedIndex(index)}>{item}
+              </li>)
+            } 
         </ul>
     </>
   );
